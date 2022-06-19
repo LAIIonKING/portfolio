485af4f40616b7fa4d5a35b739349e8e3cbc27b2
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-06-19 22:33:22 +0900

    JS:transparent navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Make navbar transparent when it is on top 
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar__dark');
+    } else{
+        navbar.classList.remove('navbar__dark');
+    }
+}) 
