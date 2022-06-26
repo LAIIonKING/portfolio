0a95dad1fadf501c1b201a7b4c0270923a6f4975
Liieonking <100991074+Liieonking@users.noreply.github.com>
2022-06-26 20:06:17 +0900

    JS: top button

--- main.js
+++ main.js
@@ -14,8 +14,24 @@ document.addEventListener('scroll', () => {
 //scrolling smooth when tapping on the navbarmenuitem
 const navbarMenu = document.querySelector('.navbar__menu');
 document.addEventListener('click', (event) => {
-    console.log(event.target.dataset.link);
     const link = event.target.dataset.link;
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
+    scrollIntoView(link);
 })
+
+
+const topbtn = document.querySelector('.top-btn');
+document.addEventListener('scroll', () => {
+    if(window.scrollY >400) {
+        topbtn.classList.add('visible');
+    }else{
+        topbtn.classList.remove('visible');
+    }
+})
+topbtn.addEventListener('click', () => {
+    scrollIntoView('#home');
+});
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+}
